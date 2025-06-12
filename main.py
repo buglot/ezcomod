091657e750763e0807e7e40e58164fb6b32e8a71
Buglot <65818720+buglot@users.noreply.github.com>
2025-06-12 20:55:06 +0700

    new

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
 from ui import UI ,BaseRoot
-
+import os
+try:
+    import requests
+except Exception as e:
+    print(e)
+    os.system("pip install requests")
 root = BaseRoot()
 app = UI(root)
 root.mainloop()

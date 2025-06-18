1e807d583c4b6488e9c74c959a612801854b004c
Buglot <65818720+buglot@users.noreply.github.com>
2025-06-18 16:36:03 +0700

    fix

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,8 @@
 from utils.syncmod.modfile import Modfile
 from utils.syncmod.modfile import ProfileMod
+from utils.download.Download import Downloader as dl
+from utils.socketsServer.typecommu import TypeCommu
 from utils.syncmod.maneger import MangagerProfile
 from utils.socketsServer.server import Server
-from utils.socketsServer.typecommu import TypeCommu
 from utils.httpserver.server import HTTPServer
 from utils.clientserver.server import ClientServer
